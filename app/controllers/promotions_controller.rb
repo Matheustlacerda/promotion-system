dd33667199f79c2693776884ecffd10e28174f16
Matheus_Lacerda <matheus_mtl@hotmail.com>
2021-03-21 20:39:36 -0300

    Corrigindo edição de cupom

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,63 +1,75 @@
 class PromotionsController < ApplicationController
-    def index
-        @promotions = Promotion.all
+  def index
+    @promotions = Promotion.all
+  end
+
+  def show
+    @promotion = Promotion.find(params[:id])
+  end
+
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    @promotion = Promotion.new(promotion_params)
+    if @promotion.save
+      redirect_to @promotion
+    else
+      render :new
+    end
+  end
+
+  def edit 
+    @promotion = Promotion.find(params[:id])
+  end
+
+  def update
+    @promotion = Promotion.find(params[:id]) 
+    if update_promotions_and_cupons
+      flash[:notice] = 'Promoção editada com sucesso'
+      redirect_to @promotion
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @promotion = Promotion.find(params[:id])
+    flash[:notice] = "Promoção #{@promotion.name} apagada! "
+    @promotion.destroy
+    redirect_to promotions_path
+  end
+
+  def generate_coupons
+    @promotion = Promotion.find(params[:id]) 
+    create_cupons_for_promotion   
+    flash[:notice] = 'Cupons gerados com sucesso'
+    redirect_to @promotion
+  end  
+
+  private 
+    def promotion_params
+      params
+        .require(:promotion)
+        .permit(:name, :expiration_date, :description,
+                :discount_rate, :code, :coupon_quantity)
     end
 
-    def show
-        @promotion = Promotion.find(params[:id])
-    end
-
-    def new
-        @promotion = Promotion.new
-    end
-
-    def create
-        @promotion = Promotion.new(promotion_params)
-        if @promotion.save
-          redirect_to @promotion
-        else
-          render :new
-        end
-    end
-
-    def edit 
-      @promotion = Promotion.find(params[:id])
-    end
-
-    def update
-      @promotion = Promotion.find(params[:id]) 
-      if @promotion.update(promotion_params)
-        flash[:notice] = 'Promoção editada com sucesso'
-        redirect_to @promotion
-      else
-        render :edit
+    def update_promotions_and_cupons
+      @promotion.update(promotion_params)
+  
+      if @promotion.coupons.any?
+        @promotion.coupons.destroy_all
+        create_cupons_for_promotion
       end
-    end
+      
+      true
+    end    
 
-    def destroy
-      @promotion = Promotion.find(params[:id])
-      flash[:notice] = "Promoção #{@promotion.name} apagada! "
-      @promotion.destroy
-      redirect_to promotions_path
-    end
-
-    def generate_coupons
-      @promotion = Promotion.find(params[:id])
-
+    def create_cupons_for_promotion
       (1..@promotion.coupon_quantity).each do |number|
         Coupon.create!(code: "#{@promotion.code}-#{'%04d' % number}", promotion: @promotion)
       end
-
-      flash[:notice] = 'Cupons gerados com sucesso'
-      redirect_to @promotion
     end
-
-    private 
-
-      def promotion_params
-        params
-          .require(:promotion)
-          .permit(:name, :expiration_date, :description,
-                 :discount_rate, :code, :coupon_quantity)
-      end
 end
