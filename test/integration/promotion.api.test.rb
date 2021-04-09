4d82b0604ba28ea0eb92d4dca3e9696056304d16
Matheus_Lacerda <matheus_mtl@hotmail.com>
2021-04-09 15:55:16 -0300

    Finalizando api

--- test/integration/promotion.api.test.rb
+++ test/integration/promotion.api.test.rb
@@ -10,10 +10,37 @@ class PromotionApiTest < ActionDispatch::IntegrationTest
                                   expiration_date: '22/12/2033', user: user)
     coupon = Coupon.create!(code: 'NATAL10-0001', promotion: promotion)
 
-    get "/api/v1/coupons/#{coupon.code}"
+    get "/api/v1/coupons/#{coupon.code}", as: :json
 
     assert_response 200
     body = JSON.parse(response.body, symbolize_names: true)
-    assert_equal coupon.code, body[:code]
+    assert_equal promotion.discount_rate.to_s, body[:discount_rate]
+  end
+
+  test 'cupon not found' do
+    get "/api/v1/coupons/0", as: :json
+
+    assert_response 404
+  end
+
+  test 'route invalid without json header' do 
+    get "/api/v1/coupons/0", as: :json
+
+    assert_response 404
+  end
+
+  test 'show cupon disable' do
+    user = User.create!(email: 'eduardo@iugu.com.br', password: 'password')
+    promotion = Promotion.create!(name: 'Natal',
+                                  description: 'Promoção de natal',
+                                  code: 'NATAL10', discount_rate: 15,
+                                  coupon_quantity: 5,
+                                  expiration_date: '22/12/2033', user: user)
+    coupon = Coupon.create!(code: 'NATAL10-0001', promotion: promotion,
+                            status: :disabled)
+
+    get "/api/v1/coupons/#{coupon.code}", as: :json
+
+    assert_response :not_found
   end
 end
